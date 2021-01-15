cf6fee31980204d546f33879f6c968a9c272863a
Vibhor Singh <vibhor.formal@gmail.com>
2021-01-15 09:02:07 +0530

    Updates

---
+++ precache-manifest.15604a59880648f9cacfb9a8470a778d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7333c0579b8d97dec2b5",
+    "url": "/f-t/static/css/main.d138d36e.chunk.css"
+  },
+  {
+    "revision": "7333c0579b8d97dec2b5",
+    "url": "/f-t/static/js/main.29a362d1.chunk.js"
+  },
+  {
+    "revision": "2305328e6ad3884385ec",
+    "url": "/f-t/static/js/runtime~main.b1ee33b6.js"
+  },
+  {
+    "revision": "f3c8403f108f8d2e86e8",
+    "url": "/f-t/static/js/2.38f62b53.chunk.js"
+  },
+  {
+    "revision": "0be6fec9ecd07391eed5afacb4b1db20",
+    "url": "/f-t/index.html"
+  }
+];
