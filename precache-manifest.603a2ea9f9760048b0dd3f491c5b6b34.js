bd11f0c76322bd7bc22a0dfa204288647951e7a1
Vibhor Singh <vibhor.formal@gmail.com>
2021-01-19 14:11:46 +0530

    Updates

---
+++ precache-manifest.603a2ea9f9760048b0dd3f491c5b6b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d8d5b9f764ea543b13",
+    "url": "/F-T-Nature/static/css/main.f6504a73.chunk.css"
+  },
+  {
+    "revision": "53d8d5b9f764ea543b13",
+    "url": "/F-T-Nature/static/js/main.b60d12ac.chunk.js"
+  },
+  {
+    "revision": "0ddaaeff703c94d0f851",
+    "url": "/F-T-Nature/static/js/runtime~main.ba7eb520.js"
+  },
+  {
+    "revision": "57fd788a910ee2fdc669",
+    "url": "/F-T-Nature/static/css/2.967e2351.chunk.css"
+  },
+  {
+    "revision": "57fd788a910ee2fdc669",
+    "url": "/F-T-Nature/static/js/2.c88f10a2.chunk.js"
+  },
+  {
+    "revision": "3a8d3f18f70515c85b6b7bcf913e0973",
+    "url": "/F-T-Nature/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/f-t/precache-manifest.16d5fb1d627e73c65714ed46b133a567.js"
+  "/F-T-Nature/precache-manifest.603a2ea9f9760048b0dd3f491c5b6b34.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/f-t/index.html", {
+workbox.routing.registerNavigationRoute("/F-T-Nature/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
